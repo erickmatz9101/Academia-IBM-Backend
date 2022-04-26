a00db05c44f221abb12298c392a664b644f6c883
Erick Martinez <erick_martinez9101@outlook.com>
2022-04-25 21:51:40 -0500

    Validado los datos del usuario

--- app-ruleta/src/com/ibm/academia/appruleta/Ruleta.java
+++ app-ruleta/src/com/ibm/academia/appruleta/Ruleta.java
@@ -1,7 +1,5 @@
 package com.ibm.academia.appruleta;
-
 import java.io.Serializable;
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
@@ -14,7 +12,8 @@ public class Ruleta implements Serializable
      private boolean ruletaAbierta;
      private double valorApertura;
      private int opcion;
-     List <String> colores = Arrays.asList("negro","rojo");
+     List<String> colores=Arrays.asList("negro", "rojo");
+     List <Integer> numeros= Arrays.asList(1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36);
 
      public Ruleta() {
      }
@@ -99,22 +98,48 @@ public class Ruleta implements Serializable
                switch (opcion){
 
                     case 1:
-                         System.out.println("En esta opcion deberas elegir un color el cual unicamente podra ser negro o rojo:\n");
+                         System.out.println("Elegiste la opcion 1: En  esta opcion deberas ingresar un color solo hay 2 opciones, negro y rojo\n");
                          Scanner eleccionColor = new Scanner(System.in);
                          color =eleccionColor.nextLine();
-
                          if (colores.contains(color)){
-                              System.out.println("El color que ingresate es: "+color);
+                              System.out.println("El color que ingresaste es: "+color);
                          }else{
-                              System.out.println("Color no valido");
+                              System.out.println("El color ingresado no es valido, intentalo de nuevo");
+                         }
+                         break;
+                    case 2:
+                         System.out.println("Elegiste la opcion 2: En esta opcion puedes apostar por un numero que esté entre el 1 y el 36 ");
+                         Scanner entradaNumero = new Scanner(System.in);
+                         numero=entradaNumero.nextInt();
+                         if (numeros.contains(numero)){
+                              System.out.println("El numero que elegiste es el : "+numero);
+                         }else{
+                              System.out.println("El numero ingresado no es valido intentalo de nuevo");
+                         }
+                         break;
+
+                    case 3:
+                         System.out.println("Elegiste la opcion 3: En esta opcion puedes apostar por un color y un numero. Ingresa el color que debera ser negro o rojo y un numero que este entre el 1 y el 36\n");
+                         Scanner eleccionColor2 = new Scanner(System.in);
+                         color =eleccionColor2.nextLine();
+                         if (colores.contains(color)){
+                              System.out.println("El color que ingresaste es: "+color);
+                         }else{
+                              System.out.println("El color ingresado no es valido, intentalo de nuevo");
                          }
 
-
-
-                         System.out.println("Ahora coloca la cantidad que deseas apostar la cual debera ser maximo de 10,000 dolares: ");
-
-
+                         System.out.println("Ahora tienes que elegir un numero que se encuentre entre el 1 y el 36 ");
+                         Scanner entradaNumero2 = new Scanner(System.in);
+                         numero=entradaNumero2.nextInt();
+                         if (numeros.contains(numero)){
+                              System.out.println("El numero que elegiste es el : "+numero);
+                         }else{
+                              System.out.println("El numero ingresado no es valido intentalo de nuevo");
+                         }
                          break;
+
+                    default:
+                         System.out.println("Ninguna de las opciones de juego es valida, intentalo mas tarde");
 
                }
 
